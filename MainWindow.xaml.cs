7ad264d0ec4dde1493b7a0e5af59183c72856849
Christopher Pina <christopherpina2022@gmail.com>
2024-03-19 19:26:27 -0500

    WPF-Conversion Init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ALAWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void groupButton_click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("you clicked the Group Button");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# ALA Windows Presentation Foundation build
+This is the same application as the ALA, but built with WPF instead of WinForms, i'm mainly doing this to learn how to implement WPF. if you want the original, you can find
+it on the main branch.
+
+## Resource Dictionary
+I found that I can make a WPF form use a similar form of a CSS file by just making a resource dictionary from a seperate file and then calling it in App.xaml
+which should apply the styles once i get them setup correctly. This can make it so I don't have to define positioning, size, and alignment every single time.
+
